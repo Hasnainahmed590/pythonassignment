3b84e9279496e0245814c7447a5b3c3d3c84bda2
Hasnainahmed590 <93081058+Hasnainahmed590@users.noreply.github.com>
2022-03-28 11:38:05 +0500

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,21 @@
+var num1 = +prompt ('first num ')
+var operator = prompt ('operator')
+var num2 = + prompt ('second num')
+
+if (operator =='+'){
+ var add =num1 + num2
+ console.log('addition = ' + add)
+}
+
+else if (operator =='-'){
+    var sub =num1 - num2
+    console.log('addition = ' + sub)
+}
+else if (operator =='*'){
+    var multiply =num1 * num2
+    console.log('addition = ' + multiply)
+}
+else if (operator =='/'){
+ var divide =num1 / num2
+ console.log('addition = ' + divide)
+}
